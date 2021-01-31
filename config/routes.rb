5e3f3fc60182f04b538c0bf909d891cb7e3dffb8
minty1202 <aki.men1202@gmail.com>
2021-02-01 08:34:31 +0900

    2/1 ログイン基礎

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :staffs
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
   root to: 'welcome#index'
