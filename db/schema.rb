ef7e498040ea7345bc2037313739bf07541be77e
ogatasaori <daigocafe@msn.com>
2021-02-03 18:21:27 +0900

    1. コンフリクト解消

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema.define(version: 2021_01_27_141603) do
+ActiveRecord::Schema.define(version: 2021_01_31_230845) do
 
   create_table "members", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,7 +23,6 @@ ActiveRecord::Schema.define(version: 2021_01_27_141603) do
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
-
 
   create_table "staffs", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.string "email", default: "", null: false
