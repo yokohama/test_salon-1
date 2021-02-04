52fb38eb66ef5f522a9343ce1d4dc00691f1bb46
minty1202 <aki.men1202@gmail.com>
2021-02-04 18:49:27 +0900

    ルーティング追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,24 @@
 Rails.application.routes.draw do
-  devise_for :members
+  devise_for :members, controllers: {
+    sessions: 'members/sessions',
+    passwords: 'members/passwords',
+    registrations: 'members/registrations'
+   }
   devise_for :staffs, controllers: {
     sessions: 'staffs/sessions',
     passwords: 'staffs/passwords'
    }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'welcome#index'
-
+  
   namespace :staffs do
     resource :dashboards, only: [:show]
     resources :members, only: [:index]
   end
-
+  
   namespace :members do
     resource :dashboards, only: [:show]
+    resources :members, only: [:index]
   end
   
 end
