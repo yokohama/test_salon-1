aeca7bb989cdefb8687779e19e9858db5f7b6b27
minty1202 <aki.men1202@gmail.com>
2021-02-08 08:30:06 +0900

    大枠作成

--- app/controllers/staffs/categories_controller.rb
+++ app/controllers/staffs/categories_controller.rb
@@ -1,7 +1,52 @@
 module Staffs
   class CategoriesController < StaffsController
+    # index, show, new, create, edit, update, destroy
     def index
+      @categories = Category.all
     end
+
+    def show
+      @category = Category.find(params[:id])
+    end
+
+    def new
+      @category = Category.new
+    end
+
+    def create
+      @category = Category.new(category_params)
+      if @category.save
+        redirect_to staffs_categories_path
+      else
+        render :new
+      end
+    end
+
+    def edit
+      @category = Category.find(params[:id])
+    end
+
+    def update
+      @category = Category.find
+      if @category.update(category_params)
+        redirect_to :show
+      else
+        render :edit
+      end
+    end
+
+    def destroy
+      @category = Category.find(params[:id])
+      @category.destroy
+      redirect_to :index
+    end
+
+    private
+
+    def category_params
+      params.require(:category).permit(:name)
+    end
+
   end
 end
 
