158b8680d1406c7458810017e27a43b66805257c
minty1202 <aki.men1202@gmail.com>
2021-02-10 18:42:26 +0900

    validates 追加

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true
   validates :description, presence: true
+  validates :category_id, presence: true
 
   belongs_to :category
 end
