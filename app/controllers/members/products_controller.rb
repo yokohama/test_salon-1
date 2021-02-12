cb12004e4b568680028a2e3a78878b85cf2060ac
minty1202 <aki.men1202@gmail.com>
2021-02-12 22:25:22 +0900

    controller 記述

--- app/controllers/members/products_controller.rb
+++ app/controllers/members/products_controller.rb
@@ -2,9 +2,12 @@ module Members
   class ProductsController < MembersController
 
     def index
+      @products = Product.includes(:category)
+      @categorys = Category.includes(:products)
     end
 
     def show
+      @product = Product.find(params[:id])
     end
   end
 end
