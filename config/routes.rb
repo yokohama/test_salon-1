5caa157f2f4e919a49eb6210c4c7d9cc83428c4a
minty1202 <aki.men1202@gmail.com>
2021-02-12 21:02:22 +0900

    routes products 追加

--- config/routes.rb
+++ config/routes.rb
@@ -21,8 +21,7 @@ Rails.application.routes.draw do
   namespace :members do
     resource :dashboards, only: [:show]
     resource :profiles, only: [:show, :edit, :update]
-    resources :products, only: [:index]
-  
+    resources :products, only: [:index, :show]
   end
 
 end
