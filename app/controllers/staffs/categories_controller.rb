45fa8494f6fedf9d0589ea1943913da0386b3c51
minty1202 <aki.men1202@gmail.com>
2021-02-14 19:33:32 +0900

    create update destroy のメッセージ追加

--- app/controllers/staffs/categories_controller.rb
+++ app/controllers/staffs/categories_controller.rb
@@ -16,8 +16,9 @@ module Staffs
     def create
       @category = Category.new(category_params)
       if @category.save
-        redirect_to staffs_categories_path
+        redirect_to staffs_categories_path, notice: 'カテゴリを登録しました'
       else
+        japan_error_message
         render :new
       end
     end
@@ -27,15 +28,16 @@ module Staffs
 
     def update
       if @category.update(category_params)
-        redirect_to staffs_category_path
+        redirect_to staffs_category_path, notice: 'カテゴリを編集しました'
       else
+        japan_error_message
         render :edit
       end
     end
 
     def destroy
       @category.destroy
-      redirect_to staffs_categories_path
+      redirect_to staffs_categories_path, alert: 'カテゴリを削除しました。'
     end
 
     private
@@ -47,6 +49,15 @@ module Staffs
     def category_find
       @category = Category.find(params[:id])
     end
+
+    def japan_error_message
+      if @category.errors.full_messages == ["Name can't be blank"]
+        flash.now[:alert] = "nameが入力されていません"
+      elsif @category.errors.full_messages == ["Name has already been taken"]
+        flash.now[:alert] = "すでに同じ名前が登録されています"
+      end
+    end
+
   end
 end
 
