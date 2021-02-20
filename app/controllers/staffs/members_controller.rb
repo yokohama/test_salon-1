be13c218cb0bdc5ed73e00cdaa3e5a3ddda366e2
minty1202 <aki.men1202@gmail.com>
2021-02-20 16:59:37 +0900

    コントローラー追加

--- app/controllers/staffs/members_controller.rb
+++ app/controllers/staffs/members_controller.rb
@@ -1,8 +1,27 @@
 module Staffs
   class MembersController < StaffsController
+    before_action :set_q, only: [:index, :search]
+
     def index
-      @members = Member.all
+      @members = Member.all.page(params[:page]).per(5)
     end
+
+    def search
+      if params[:q]['id_eq'] == "" && params[:q]['nickname_cont'] == "" && params[:q]['email_cont'] == ""
+        redirect_to staffs_members_path
+      elsif @q.result.present?
+        @results = @q.result.page(params[:page]).per(5)
+      else
+        redirect_to staffs_members_path
+      end
+    end
+
+  private
+
+  def set_q
+    @q = Member.ransack(params[:q])
+  end
+    
   end
 end
 
