4b69f0d9c06fce5a74ce088cfe2f831e405272f0
minty1202 <aki.men1202@gmail.com>
2021-02-20 17:05:58 +0900

    検索結果表示

--- app/controllers/staffs/members_controller.rb
+++ app/controllers/staffs/members_controller.rb
@@ -2,7 +2,7 @@ module Staffs
   class MembersController < StaffsController
 
     def index
-          @q = Member.ransack(params[:q])
+      @q = Member.ransack(params[:q])
       @members = @q.result.page(params[:page]).per(5)
       @results = @q.result
     end
