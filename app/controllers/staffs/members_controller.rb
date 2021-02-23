805bdf037dd69d0f8bf9966a41aa6f7310430e85
minty1202 <aki.men1202@gmail.com>
2021-02-23 12:07:56 +0900

    indexに検索機能を追加する

--- app/controllers/staffs/members_controller.rb
+++ app/controllers/staffs/members_controller.rb
@@ -1,28 +1,12 @@
 module Staffs
   class MembersController < StaffsController
-    before_action :set_q, only: [:index, :search]
 
     def index
+          @q = Member.ransack(params[:q])
       @members = @q.result.page(params[:page]).per(5)
+      @results = @q.result
     end
 
-    def search
-      if params[:q]['id_eq'] == "" && params[:q]['nickname_cont'] == "" && params[:q]['email_cont'] == ""
-        redirect_to staffs_members_path, alert: t('.blank')
-      elsif @q.result.present?
-        @results = @q.result.page(params[:page]).per(5)
-      else
-        # raise t('.result').inspect
-        redirect_to staffs_members_path, alert: t('.no_result')
-      end
-    end
-
-  private
-
-  def set_q
-    @q = Member.ransack(params[:q])
-  end
-    
   end
 end
 
